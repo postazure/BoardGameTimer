a968f13c0ad079c026057ad097548ece983b2b6f
Max Gordon <gordonmaxc@gmail.com>
2016-05-24 09:04:27 -0700

    Cleaner event triggers based on brightness

--- BoardGameTimer.ino
+++ BoardGameTimer.ino
@@ -5,9 +5,6 @@ int greenLed = 9;
 int redLed = 10;
 int blueLed = 11;
 
-int maxLight = 0;
-int minLight = 0;
-
 int totalPlayers = 4;
 float playerTimes[4] = {0, 0, 0, 0};
 int currentPlayer = 0;
@@ -19,6 +16,7 @@ bool wasPaused = true;
 bool wasTiming = false;
 
 int lastPassingSensorValue = 1000;
+int maxLight = 0;
 
 int * currentPlayerColor;
 
@@ -41,32 +39,47 @@ void setup() {
 
 void loop() {
   int brightness = getBrightness();
-  if (brightness > maxLight * 0.90) {
-    //    faceup | pause
-    Serial.println("Paused");
-    currentPlayer = resumeOnPlayer;
-    wasPaused = true;
-    wasPassing = false;
-    wasTiming = false;
 
-    flashPlayerColor(currentPlayerColor, 100);
-//  } else if (!wasPaused && brightness > maxLight * 0.15 && getBrightness() + maxLight * 0.05 > lastPassingSensorValue) {
-  } else if (!wasPaused && brightness > maxLight * 0.30) {
-    //    passing?
-   
-    wasPassing = true;
-    wasPaused = false;
-    wasTiming = false;
-    
-    
-    rgbOff();
-    Serial.println("Passing");
-    
+// minLight ---timing---|----passing----|---paused---maxLight
 
-  } else if (!wasTiming && minLight - getBrightness() < maxLight * 0.15) {
-    
-    Serial.print("Timing ");
+  if (brightness < maxLight * 0.05) {
     //    facedown | players turn
+    timingSeq();
+  } else if (brightness > maxLight * 0.95) {
+    //    faceup | pause
+    pausedSeq();
+  } else {
+     //    passing?
+   passingSeq();
+  }
+  
+
+
+
+  
+//  if (brightness > maxLight * 0.90) {
+//    //    faceup | pause
+//    pausedSeq();
+////  } else if (!wasPaused && brightness > maxLight * 0.15 && getBrightness() + maxLight * 0.05 > lastPassingSensorValue) {
+//  } else if (!wasPaused && brightness > maxLight * 0.30) {
+//    //    passing?
+//   passingSeq();
+//    
+//  } else if (!wasTiming && minLight - getBrightness() < maxLight * 0.15) {
+//    //    facedown | players turn
+//    timingSeq();
+//  }
+
+  lastPassingSensorValue = getBrightness();
+  Serial.print("current: ");
+  Serial.print(getBrightness());
+  Serial.print(" max: ");
+  Serial.println(maxLight);
+}
+
+void timingSeq(){
+  Serial.print("Timing ");
+    
     if (wasPassing) {
       Serial.println("| New Player");
       nextPlayer();
@@ -83,17 +96,25 @@ void loop() {
     wasPaused = false;
     wasTiming = true; 
     wasPassing = false;
-    
-  }
-
-  lastPassingSensorValue = getBrightness();
-  Serial.print("current: ");
-  Serial.print(getBrightness());
-  Serial.print(" min: ");
-  Serial.print(minLight);
-  Serial.print(" max: ");
-  Serial.println(maxLight);
 }
 
-//resuming from pause doesnt work
+void passingSeq(){
+  wasPassing = true;
+    wasPaused = false;
+    wasTiming = false;
+    
+    
+    rgbOff();
+    Serial.println("Passing");
+}
+
+void pausedSeq(){
+  Serial.println("Paused");
+    currentPlayer = resumeOnPlayer;
+    wasPaused = true;
+    wasPassing = false;
+    wasTiming = false;
+
+    flashPlayerColor(currentPlayerColor, 100);
+}
 
--- SensorController.ino
+++ SensorController.ino
@@ -1,18 +1,12 @@
+
+
 int getBrightness(){
   return analogRead(sensorPin);
 }
 
 void calibrateLightSensor() {
-  Serial.println("calibrating/dark");
-  while(millis() < 2000){
-    rgbFlash(0,0,255,50);
-    minLight = analogRead(sensorPin);
-  } 
-  
-  rgbFlash(0,255,0,500, 4);
-
   Serial.println("calibrating/bright");
-  while(millis() < 8000){
+  while(millis() < 3000){
     rgbFlash(0,0,255,50);
     maxLight = analogRead(sensorPin);
   }
